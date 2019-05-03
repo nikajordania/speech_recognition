13f1cd5ed1d9aa8a5d0195a82e8cd9631b82a047
nikajordania <nika.zhordania.1@btu.edu.ge>
2019-05-03 17:57:17 +0400

    add sound

--- speech.py
+++ speech.py
@@ -1,20 +1,24 @@
 import speech_recognition as sr
 import os
+import pyttsx3
 record = sr.Recognizer()
 microphone = sr.Microphone()
 
 def runProgramm(text):
     if str(text) == "google":
-        os.system("start C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
+        os.system("start C:\Program Files (x86)\Google\Chrome\Application\chrome.lnk")
 
 try:
     while True:
-        with microphone as source:
-            record.adjust_for_ambient_noise(source)
-            audio = record.listen(source)
-            result=record.recognize_google(audio,language="en_EN")
+        with microphone:
+            record.adjust_for_ambient_noise(microphone)
+            audio = record.listen(microphone)
+            result = record.recognize_google(audio, language="ru_RU")
             result = result.lower()
             print(format(result))
+            engine = pyttsx3.init()
+            engine.say(format(result))
+            engine.runAndWait()
             runProgramm(format(result))
 except sr.UnknownValueError:
-    print('Сервис google не отвечает')
+    print("google не отвечает")
