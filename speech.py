b574586a44cf57af2744cb69054f198b02270b08
nikajordania <nika.zhordania.1@btu.edu.ge>
2019-05-04 03:19:15 +0400

    update
    cods daemata ramdenime cvlilebebi

--- speech.py
+++ speech.py
@@ -1,24 +1,43 @@
 import speech_recognition as sr
-import os
+import sys
+import webbrowser
 import pyttsx3
-record = sr.Recognizer()
+
+r = sr.Recognizer()
 microphone = sr.Microphone()
 
-def runProgramm(text):
-    if str(text) == "google":
-        os.system("start C:\Program Files (x86)\Google\Chrome\Application\chrome.lnk")
+def talk(words):
+    engine = pyttsx3.init()
+    engine.say(words)
+    engine.runAndWait()
 
-try:
-    while True:
-        with microphone:
-            record.adjust_for_ambient_noise(microphone)
-            audio = record.listen(microphone)
-            result = record.recognize_google(audio, language="ru_RU")
-            result = result.lower()
-            print(format(result))
-            engine = pyttsx3.init()
-            engine.say(format(result))
-            engine.runAndWait()
-            runProgramm(format(result))
-except sr.UnknownValueError:
-    print("google не отвечает")
+def command():
+	with microphone as source:
+		print("say")
+		r.pause_threshold = 1
+		r.adjust_for_ambient_noise(source)
+		audio = r.listen(source)
+
+	try:
+		voice = r.recognize_google(audio, language="en_EN")
+		voice = voice.lower()
+		print("you said: {}".format(voice))
+
+	except sr.UnknownValueError:
+		talk("I did not understand")
+		voice = command()
+	return voice
+
+def makeSomething(voice):
+	if voice == "open site":
+		talk("opening")
+		url = "https://www.google.com/"
+		webbrowser.open(url)
+	elif voice == "stop":
+		talk("Yes of course")
+		sys.exit()
+	elif voice =="what is your name":
+		talk("my name is siri")
+
+while True:
+	makeSomething(command())
